9b17bd7a851a68bc84027c8ceaebf25202de8be3
konapidolpoulos <konapidopoulos@gmail.com>
2020-05-01 23:40:55 +0300

    copyright enchansments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule, ExtraOptions} from '@angular/router';
 import {AppComponent} from './app.component';
 
-const routerOptions: ExtraOptions = {
-  scrollPositionRestoration: 'enabled',
-  anchorScrolling: 'enabled',
-  scrollOffset: [0, 64]
-};
-
 const routes: Routes = [
   {path: '', component: AppComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, routerOptions)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
