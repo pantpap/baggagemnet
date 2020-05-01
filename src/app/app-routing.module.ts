0fece4f85644fcc20f2c6579f6f9557a7f4c8f76
konapidolpoulos <konapidopoulos@gmail.com>
2020-05-01 23:24:21 +0300

    scroll to top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, ExtraOptions} from '@angular/router';
+import {AppComponent} from './app.component';
 
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64]
+};
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
@@ -10,12 +10,20 @@ export class MainComponent implements OnInit {
   url = 'http://baggagement.com/book/?widget';
   urlSafe: SafeResourceUrl;
   @ViewChild('engine', { static: true }) engine;
-
+  @ViewChild('main', { static: true }) main;
   constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
     console.log(this.engine);
+  }
+
+  scrollTo() {
+    window.scroll({
+      top: 250,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 
 }
