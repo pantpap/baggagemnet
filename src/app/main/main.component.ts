af0a933177d37e595ba52e620f7915fbf132214e
Pantelis Papasavvas <pantpap@gmail.com>
2020-05-13 01:13:50 +0300

    book a tranfer button now appears when you scroll down above the booking engine in a mobile device

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
@@ -11,12 +11,29 @@ export class MainComponent implements OnInit {
   urlSafe: SafeResourceUrl;
   @ViewChild('engine', { static: true }) engine;
   @ViewChild('main', { static: true }) main;
+  @ViewChild('goBooking', { static: true }) goBooking;
+
+  scrolled = 0;
+
+
+
   constructor(public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
-    console.log(this.engine);
   }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll($event) {
+    const scrollY = window.scrollY;
+    if ( scrollY >= 300) {
+      this.scrolled = 1;
+    } else {
+      this.scrolled = 0;
+    }
+
+  }
+
 
   scrollTo() {
     window.scroll({
